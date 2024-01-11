93a600c3d87a8c6dc9fb3056fb934a821b8313ce
Said Mounaim <saidmounaim00@gmail.com>
2024-01-11 14:48:18 +0100

    Add Job Filter Sidebar Component

---
+++ app/components/JobFilterSidebar.tsx
@@ -0,0 +1,71 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Select from "@/components/ui/select";
+import prisma from "../../lib/prisma";
+import { jobTypes } from "@/lib/job-types";
+import { Button } from "@/components/ui/button";
+
+async function filterJobs(formData: FormData) {
+  "use server";
+}
+
+const JobFilterSidebar = async () => {
+  const allLocation = (await prisma?.job
+    .findMany({
+      where: { approved: true },
+      select: { location: true },
+      distinct: ["location"],
+    })
+    .then((location) =>
+      location.map(({ location }) => location).filter(Boolean),
+    )) as string[];
+
+  return (
+    <aside className="sticky top-0 h-fit rounded-md border bg-background p-4 md:w-[260px]">
+      <form action={filterJobs}>
+        <div className="space-y-4">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="q">Search</Label>
+            <Input id="q" name="q" placeholder="Title, company, etc" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="types">Types</Label>
+            <Select id="types" name="types" defaultValue="">
+              <option>All types</option>
+              {jobTypes.map((type) => (
+                <option key={type} value={type}>
+                  {type}
+                </option>
+              ))}
+            </Select>
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="location">Location</Label>
+            <Select id="location" name="location" defaultValue="">
+              <option>All locations</option>
+              {allLocation.map((location) => (
+                <option key={location} value={location}>
+                  {location}
+                </option>
+              ))}
+            </Select>
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              id="remote"
+              type="checkbox"
+              name="remote"
+              className="scale-125 accent-black"
+            />
+            <Label htmlFor="remote">Remote jobs</Label>
+          </div>
+          <Button type="submit" className="w-full">
+            Filter jobs
+          </Button>
+        </div>
+      </form>
+    </aside>
+  );
+};
+
+export default JobFilterSidebar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import prisma from "../lib/prisma";
 import JobCard from "./components/JobCard";
+import JobFilterSidebar from "./components/JobFilterSidebar";
 
 async function getJobs() {
   const jobs = await prisma?.job.findMany({
@@ -13,14 +14,15 @@ export default async function Home() {
   const jobs = await getJobs();
 
   return (
-    <main className="max-w-5xl mx-auto px-3 my-10 space-y-10">
+    <main className="mx-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5">
         <h1 className="text-3xl font-extrabold tracking-tight lg:text-5xl">
           Developer jobs
         </h1>
         <p className="text-muted-foreground">Find your dream job.</p>
       </div>
-      <section>
+      <section className="flex flex-col gap-4 md:flex-row">
+        <JobFilterSidebar />
         <div className="space-y-4">
           {jobs.map((job) => (
             <JobCard key={job.id} job={job} />
